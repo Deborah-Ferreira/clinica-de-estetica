5391f99c8f451c24b7e264accdccedcc62278eed
Deborah-Ferreira <deborajfd@gmail.com>
2025-05-23 02:00:28 -0300

    feat: add about-philosophy section with animation

---
+++ script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver(entradas => {
+  entradas.forEach(entrada => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add('visivel');
+      observer.unobserve(entrada.target);
+    }
+  });
+});
+
+const elementos = document.querySelectorAll('.animation');
+
+elementos.forEach(el => 
+observer.observe(el));
