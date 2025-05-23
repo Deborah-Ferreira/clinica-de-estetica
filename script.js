7d0b6d592eafe1125af6de60ab05db318a945655
Deborah-Ferreira <deborajfd@gmail.com>
2025-05-23 16:48:03 -0300

    fix: animation now triggers only when in viewport

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const observer = new IntersectionObserver(entradas => {
   entradas.forEach(entrada => {
     if (entrada.isIntersecting) {
-      entrada.target.classList.add('visivel');
+      entrada.target.classList.add('visivel'); 
       observer.unobserve(entrada.target);
     }
   });
 });
 
-const elementos = document.querySelectorAll('.animation');
+const elementos = document.querySelectorAll('.animation'); 
 
 elementos.forEach(el => 
-observer.observe(el));
+  observer.observe(el));  
